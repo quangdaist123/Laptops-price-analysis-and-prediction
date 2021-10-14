65ab70b8fc56488e6544bbcb515489206f3e3c07
Lov <quangdaist01@gmail.com>
2021-10-14 08:53:24 +0700

    inherit from abstract base class

--- Crawl/DMX/crawl_from_dmx.py
+++ Crawl/DMX/crawl_from_dmx.py
@@ -1,22 +1,17 @@
-import json
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 from Crawl.DMX.utils import convert
+from Crawl.base_class import BaseScraper
 
 
-class DienMayXanhScraper:
-    """
-    Parse comments and ratings of Iphones on
-    https://www.dienmayxanh.com/dien-thoai-apple-iphone
-    """
+# %%
 
+class DienMayXanhScraper(BaseScraper):
     def __init__(self):
-        self.driver = self._load_driver()
+        super().__init__()
         self.driver.get("https://www.dienmayxanh.com/laptop/")
         try:
             while True:
@@ -24,37 +19,6 @@ class DienMayXanhScraper:
                 self.driver.find_element_by_class_name("view-more").click()
         except:
             self.laptops = self.driver.find_elements_by_xpath('//*[@id="categoryPage"]/div[3]/ul/li/a[1]')
-
-    @staticmethod
-    def _load_driver():
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-        _chrome_options = ChromeOptions()
-        _chrome_options.add_argument(f"user-agent={user_agent}")
-        # _chrome_options.add_argument("--headless")
-        _chrome_options.add_argument("--disable-extensions")
-        _chrome_options.add_argument("--incognito")
-        _chrome_options.add_argument("--window-size=1920x1080")
-        driver = webdriver.Chrome(options=_chrome_options, executable_path="Crawl/DMX/utils/chromedriver_dai.exe")
-        return driver
-
-    def _go_to_first_tab(self) -> None:
-        self.driver.switch_to.window(self.driver.window_handles[0])
-
-    def _go_to_new_tab(self, *args, link=None) -> None:
-        self.driver.execute_script(f'''window.open("{link}","new_window");''')
-        self.driver.switch_to.window(self.driver.window_handles[1])
-
-    @staticmethod
-    def _append_jsonl_file(data: dict) -> None:
-        with open("results.jsonl", "a+", encoding="utf8") as f:
-            json.dump(data, f, ensure_ascii=False)
-            f.write("\n")
-
-    @staticmethod
-    def _log_errors(data: str) -> None:
-        with open("log.txt", "a+", encoding="utf8") as f:
-            f.write(data)
-            f.write("\n")
 
     def parse(self, *args, export=False) -> None:
         for i, phone in enumerate(self.laptops):
@@ -90,7 +54,7 @@ class DienMayXanhScraper:
 bot = DienMayXanhScraper()
 bot.parse(export=True)
 
-#%%
+# %%
 
 raw_data = convert.read_results("Crawl/DMX/raw/results.jsonl")
 max_columns = convert.get_spec_fields(raw_data)
