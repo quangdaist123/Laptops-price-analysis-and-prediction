1e32afaf438b34c8961c2fc390c9705365a95f08
Lov <quangdaist01@gmail.com>
2021-11-08 13:55:18 +0700

    new preprocessing

--- Preprocessing/step_3_correct_dtypes.py
+++ Preprocessing/step_3_correct_dtypes.py
@@ -36,27 +36,33 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('../Dataset/Tidy/2_dataset_filtered_missing.csv')
+df = pd.read_csv('./Dataset/Tidy/2_dataset_filtered_missing.csv')
 # %%
 
-df['cpu_speed'] = df['cpu_speed'].astype('object').replace(np.nan, 'None')
-df['max_cpu_speed'] = df['max_cpu_speed'].astype('object').replace(np.nan, 'None')
-df['ram'] = df['ram'].astype('object').replace(np.nan, 'None')
-df['ram_speed'] = df['ram_speed'].astype('object').replace(np.nan, 'None')
-df['storage'] = df['storage'].astype('object').replace(np.nan, 'None')
-df['screen_size'] = df['screen_size'].astype('object').replace(np.nan, 'None')
-df['num_sd_slot'] = df['num_sd_slot'].astype('object').replace(np.nan, 'None')
-df['bluetooth_tech'] = df['bluetooth_tech'].astype('object').replace(np.nan, 'None')
-df['released'] = df['released'].astype('object').replace(np.nan, 'None')
-df['has_lightning'] = df['has_lightning'].astype('object').replace(np.nan, 'None')
-df['has_thundebolt'] = df['has_thundebolt'].astype('object').replace(np.nan, 'None')
-df['has_touchscreen'] = df['has_touchscreen'].astype('object').replace(np.nan, 'None')
-df['has_fingerprint'] = df['has_fingerprint'].astype('object').replace(np.nan, 'None')
-df['has_camera_lock'] = df['has_camera_lock'].astype('object').replace(np.nan, 'None')
-df['has_180_degree'] = df['has_180_degree'].astype('object').replace(np.nan, 'None')
-df['has_face_id'] = df['has_face_id'].astype('object').replace(np.nan, 'None')
-df['has_antiglare'] = df['has_antiglare'].astype('object').replace(np.nan, 'None')
-df['new_warranty'] = df['new_warranty'].astype('object').replace(np.nan, 'None')
-df['used_warranty'] = df['used_warranty'].astype('object').replace(np.nan, 'None')
+df['cpu_speed'] = df['cpu_speed'].astype('object')
+df['max_cpu_speed'] = df['max_cpu_speed'].astype('object')
+df['ram'] = df['ram'].astype('object')
+df['ram_speed'] = df['ram_speed'].astype('object')
+df['storage'] = df['storage'].astype('object')
+df['screen_size'] = df['screen_size'].astype('object')
+df['num_sd_slot'] = df['num_sd_slot'].astype('object')
+df['bluetooth_tech'] = df['bluetooth_tech'].astype('object')
+df['released'] = df['released'].astype('object')
+df['has_lightning'] = df['has_lightning'].astype('object')
+df['has_thundebolt'] = df['has_thundebolt'].astype('object')
+df['has_touchscreen'] = df['has_touchscreen'].astype('object')
+df['has_fingerprint'] = df['has_fingerprint'].astype('object')
+df['has_camera_lock'] = df['has_camera_lock'].astype('object')
+df['has_180_degree'] = df['has_180_degree'].astype('object')
+df['has_face_id'] = df['has_face_id'].astype('object')
+df['has_antiglare'] = df['has_antiglare'].astype('object')
+df['new_warranty'] = df['new_warranty'].astype('object')
+df['used_warranty'] = df['used_warranty'].astype('object')
+
+# %% Chuyển các giá trị np.nan trong các cột object
+# thành 'None' để đồng nhất kiểu dữ liệu
+
+for column in df.select_dtypes(include=['object']).columns:
+    df[column] = df[column].replace(np.nan, 'None')
 
 df.to_csv('Dataset/Tidy/3_dataset_dtypes_corrected.csv', index=False)
