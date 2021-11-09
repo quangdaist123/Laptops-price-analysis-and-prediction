67ff03885b76bf303c3963a2baf5e356288eede3
Lov <quangdaist01@gmail.com>
2021-11-09 14:03:55 +0700

    change crawling order

--- Scrape/TGDD_used/crawl_TGDD_used.py
+++ Scrape/TGDD_used/crawl_TGDD_used.py
@@ -14,23 +14,33 @@ class TGDD_Scraper(BaseScraper):
         except:
             self.laptops = self.driver.find_elements_by_css_selector('.products > li')
 
-    def _parse_all_(self, sub_products, base_info):
+    def _parse_all_(self, sub_products):
+
         results = []
+
+        name = self.driver.find_element_by_css_selector(".titleimei > h1").text
         for product in sub_products:
             try:
                 self._wait_to_be_clickable(".products > li")
-                result = {}
 
-                # Lấy tgian bảo hành còn lại + tình trạng của từng máy cũ
+                result = {}
+                result['Tên'] = name
+                result["Giá máy cũ"] = product.find_element_by_css_selector("div:nth-child(2)").text
+
+                # Trích tiết kiệm
+                discount = product.find_elements_by_css_selector('li > label:nth-child(3) > span')
+                if discount:
+                    result["Tiết kiệm"] = discount[0].text
+                else:
+                    result["Tiết kiệm"] = ''
+
+                # Lấy thời gian bảo hành còn lại
                 used_info = product.find_elements_by_tag_name("label")
                 result["Bảo hành cũ"] = ""
-
                 for info in used_info:
                     if "bảo hành:" in info.text.lower():
                         result["Bảo hành cũ"] = info.text
 
-                result["Giá máy cũ"] = product.find_element_by_css_selector("div:nth-child(2)").text
-                result.update(base_info)
                 results.append(result)
             except BaseException as e:
                 # raise e
@@ -38,21 +48,17 @@ class TGDD_Scraper(BaseScraper):
                 results.append(failed_laptop)
         return results
 
-    def _parse_base_info(self, laptop):
-        base_info = {}
-        base_info["Tên"] = laptop.find_element_by_tag_name("h3").text
-        base_info["Giá máy mới"] = laptop.find_element_by_tag_name("label").text
-
-        savings = laptop.find_elements_by_css_selector("a > label:nth-child(6) > span")
-        if savings:
-            base_info["Tiết kiệm"] = savings[0].text
-
-        self._go_to_new_tab(link=laptop.find_element_by_tag_name("a").get_attribute("href"))
-
-        # Chuyển sang tab máy mới để:
-        # - Lấy bảo hành mới
+    def _parse_base_info(self):
+        # Chuyển sang tab máy mới
         new_laptop_link = self.driver.find_element_by_link_text("Xem chi tiết máy mới").get_attribute("href")
         self._go_to_new_tab(link=new_laptop_link)
+
+        # Lấy giá máy mới
+        new_price = self.driver.find_elements_by_css_selector('.box-price-present')
+        base_info = {}
+        base_info["Giá máy mới"] = new_price[0].text if new_price else ''
+
+        # Lấy bảo hành của máy mới
         try:
             warranty = self.driver.find_element_by_css_selector(
                 "ul.policy__list > li:nth-child(2) > p > b").text
@@ -67,14 +73,13 @@ class TGDD_Scraper(BaseScraper):
             # print("Không tìm thấy thông tin bảo hành")
             print("")
 
-        # - Lấy lấy phần trăm tiết kiệm để tính giá mới
-        # print(base_info["Bảo hành mới"])
+        # Lấy phần trăm + số tiền tiết kiệm
         if "ngừng" in base_info["Giá máy mới"]:
-
             discount = self.driver.find_elements_by_css_selector(".box_oldproduct > a > i > b")
             if discount:
                 base_info["Tiết kiệm"] = discount[0].text
 
+        # Lấy thông tin cấu hình
         specs = self._parse_similar_specifications()
         base_info.update(specs)
 
@@ -98,15 +103,18 @@ class TGDD_Scraper(BaseScraper):
     def parse(self, *args, export=False) -> None:
         num_laptops = len(self.laptops)
         for index, laptop in enumerate(self.laptops):
-            if index == 2:
-                print(f'Đã crawl xong 2 loại máy laptops.\n Cám ơn thầy đã xem')
-                break
+            # if index == 2:
+            # self.driver.close()
+            # print(f'Đã crawl xong 2 loại máy laptops.\n Cám ơn thầy đã xem')
+            # break
             try:
                 print(f'Crawling {index + 1}/{num_laptops}...')
-                base_info = self._parse_base_info(laptop)
+                self._go_to_new_tab(link=laptop.find_element_by_tag_name("a").get_attribute("href"))
                 sub_products = self.driver.find_elements_by_css_selector(".products > li")
-                results = self._parse_all_(sub_products, base_info)
-                self._write(results)
+                used_laptop_infos = self._parse_all_(sub_products)
+                new_laptop_info = self._parse_base_info()
+                result = self.combine_info(used_laptop_infos, new_laptop_info)
+                self._write(result)
 
             except BaseException as e:
                 # raise e
@@ -122,6 +130,13 @@ class TGDD_Scraper(BaseScraper):
                 self._append_jsonl_file("TGDD_used.jsonl", result)
             elif isinstance(result, str):
                 self._log_errors("TGDD_used_log.txt", result)
+
+    @staticmethod
+    def combine_info(used_laptop_infos, new_laptop_info):
+        used_laptop_infos = used_laptop_infos.copy()
+        for info in used_laptop_infos:
+            info.update(new_laptop_info)
+        return used_laptop_infos
 
 
 bot = TGDD_Scraper()
